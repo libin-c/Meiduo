06f85774a549587d9b15fdf69b02f1e2300d46a8
libin <84045407@qq.com>
2019-05-28 11:32:08 +0800

    从头开始

--- meiduo/celery_tasks/sms/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery_tasks.main import celery_app
-
-
-@celery_app.task
-def ccp_send_sms_code(mobile, sms_code):
-    from libs.yuntongxun.sms import CCP
-    send_result = CCP().send_template_sms(mobile, [sms_code, 5], 1)
-    print("当前验证码是:", sms_code)
-    return send_result
